0bd1c1c9091372b2bff85c7df024128caff9e580
LKD70 <lkdornan70@gmail.com>
2017-08-02 18:10:59 +0100

    Edited Start and Help commands content

--- plugins/help.js
+++ plugins/help.js
@@ -1,25 +1,20 @@
 'use strict'
 
+const emoji = require('../emoji');
 
 exports.init = (bot, prefs) => {
 
     const help = [
         "Use `/help <command>` to get help for one of the /commands.",
-        // todo: explain <> []
     ].join('\n')
 
     bot.register.command('start', {
         fn: (msg) => {
-            msg.reply.text(help, {parseMode: 'markdown'});
+            msg.reply.text("*Welcome to The Engine " + msg.from.first_name + "!*\n\nThe Engine is a powerful Telegram [Node.js](https://nodejs.org) bot based on Mattata.\nThe bot is fully modular in that it relies purely on plugins to function; This means you can add plugins to include additional functionality.\n\nWhere to go from here?\nTry the /help command for some assistance.\nOr check out some other /commands", {parseMode: 'markdown'});
         }
     });
 
     bot.register.command('help', {
-        // help: [
-        //     "That's how to use help :)",
-        //     "You can get help about other /commands too,",
-        //     "by typing `/help <command>.`",
-        // ].join('\n'),
         help,
         fn: msg => {
             var command = msg.args.toLowerCase();
@@ -32,13 +27,13 @@ exports.init = (bot, prefs) => {
                             asReply: msg.chat.type !== 'private',
                         });
                     } else {
-                        return "There's no help yet for the " + command + " command, sorry.";
+                        return "There's no help for the '" + command + "' command yet, sorry.";
                     }
                 } else {
-                    return "The command " + command + " wasn't found.";
+                    return "The command '" + command + "' wasn't found. Use /commands to list all available commands.";
                 }
             } else {
-                return msg.reply.text(help, {
+                return msg.reply.text("Hey *" + msg.from.first_name + "!* Welcome to The Engine " + emoji.get('grin') + "\n\nThe Engine is an intelligent plugin-based bot.\nAt its core, The Engine is a simple yet powerful plugin platform. This makes expanding the bots functionality as simple as adding a plugin.\nTo see all loaded plugins, try the /plugins command!\n\nNeed help for a specific command? " + help, {
                     parseMode: 'markdown',
                     asReply: msg.chat.type !== 'private',
                 });
